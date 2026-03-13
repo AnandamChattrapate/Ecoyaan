ed571cbbcc1db4a4cc98fbb90626b75f96dba3d4
Anandam Chattrapate <23eg105e05@anurag.edu.in>
2026-03-13 20:22:07 +0530

    1.2.0

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear()
+
   return (
-    <div>Footer</div>
+    <footer className="border-t mt-12 py-6 px-4 text-center text-gray-500 text-sm">
+      <p>© {currentYear} Ecoyaan. All rights reserved.</p>
+      <p className="text-xs mt-1">Sustainability made easy</p>
+    </footer>
   )
 }
